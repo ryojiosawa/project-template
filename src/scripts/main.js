e067a6b9d33c5b7303a4fafa792b1ce4d6ebb80e
Ryoji Osawa <ryojiosawa@gmail.com>
2014-07-15 12:54:34 -0700

    test Travis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,12 @@
 /**
  * Created by rosawa on 7/15/14.
  */
-console.log("Hello World");
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.say = function() {
+    console.log("Hi " + this.name);
+};
+
+var john = new Person("John");
+john.say();
